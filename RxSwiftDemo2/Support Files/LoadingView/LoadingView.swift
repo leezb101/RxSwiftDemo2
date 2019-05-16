031b2ae433452bf10ac23615dd843410c7d94229
Leezb101 <leezb101@126.com>
2019-05-16 19:39:28 +0800

    。。。

---
+++ RxSwiftDemo2/Support Files/LoadingView/LoadingView.swift
@@ -0,0 +1,21 @@
+//
+//  LoadingView.swift
+//  RxSwiftDemo2
+//
+//  Created by LeeZB on 2019/5/16.
+//  Copyright © 2019 Leezb. All rights reserved.
+//
+
+import UIKit
+
+class LoadingView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
